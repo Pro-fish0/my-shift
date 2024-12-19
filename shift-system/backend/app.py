df72ffdf9576bd817eed6bd73217b1626a94f135
M <1095853105@srca.org.sa>
2024-12-19 15:49:44 +0300

    update

--- shift-system/backend/app.py
+++ shift-system/backend/app.py
@@ -15,8 +15,8 @@ from flask import make_response  # Add this import
 app = Flask(__name__)
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://209.38.41.138","http://127.0.0.1","http://localhost"],
-        "methods": ["GET", "POST", "OPTIONS"],
+        "origins": ["http://209.38.41.138","http://127.0.0.1"],
+        "methods": ["GET", "POST", "OPTIONS","PUT"],
         "allow_headers": ["Content-Type"]
     }
 })
